a79f3f09d575cc4625ac021e6bacd70784cd3a49
V@runDby <135414171+varundubey16@users.noreply.github.com>
2026-07-12 17:19:24 +0530

    Create edgedetect.py

---
+++ edgedetect.py
@@ -0,0 +1,4 @@
+#Edges Detector & Counter built using Sobel, Laplacian & Gamma Correction & Image Enhancer using PIL for Image Processing -Script
+#removed code
+#link to colab script added -
+https://colab.research.google.com/drive/1dwHmyQM-9h5sTGzTwxB7rKS5z9IcUXlF?usp=sharing
